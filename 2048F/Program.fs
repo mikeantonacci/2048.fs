07c8d9fbd40077c2022f09448a9aaf78b31fcd91
michael antonacci <michaelantonacci2@gmail.com>
2015-02-02 21:44:58 -0500

    more whitespace, parens, etc changes

--- 2048F/Program.fs
+++ 2048F/Program.fs
@@ -1,13 +1,19 @@
-﻿type row = int option list
+﻿type cell = int option
+type row = cell list
 type board = row list
 
 let start : board = [[None;None;None;None];
                      [None;None;None;None];
                      [None;None;None;None];
                      [None;None;None;None];]
-let DIM : int = 4
-let rec pad n xs = if List.length xs = n then xs else pad n (None::xs)
+let SIZE : int = 4
+
+let rec pad n xs = if List.length xs = n 
+                   then xs 
+                   else pad n (None::xs)
+
 let shift n = pad n << List.filter (fun (x: int option) -> x <> None) 
+
 let rec merge a 
     = match a with
         | [] -> []
@@ -15,38 +21,48 @@ let rec merge a
         | (x :: y :: xs) -> if x = y 
                             then None :: Option.map ((*) 2) x :: merge xs 
                             else x :: merge (y :: xs)
+
 let move : row -> row 
-    = shift DIM << List.rev << merge << List.rev << shift DIM
+    = (shift <| SIZE) << List.rev << merge << List.rev << (SIZE |> shift)
+
 let rec transpose board
     = match board with
         | [[];[];[];[]] -> []
-        | _ -> (List.map List.head board) :: transpose (List.map List.tail board)
-let moveRight = List.map <| move
-let moveLeft = List.map <| (List.rev << move << List.rev)
+        | _ -> List.map List.head board :: transpose (List.map List.tail board)
+
+let moveRight = move |> List.map
+let moveLeft = List.rev << move << List.rev |> List.map
+let moveUp = transpose >> moveLeft >> transpose
 let moveDown = transpose << moveRight << transpose
-let moveUp = transpose << moveLeft << transpose
-let optFormat x
+
+let cellFormat x
     = match x with 
         | Some n -> sprintf "%-4i" n 
         | None -> "    "
-let rowformat = List.reduce (fun x y -> x+"|"+y) << List.map optFormat
+
+let rowformat = List.reduce (fun x y -> x+"|"+y) << List.map cellFormat
+
 let rowEmpty n ns
     = List.mapi (fun i x -> match x with 
                               | None -> (n,Some i) 
                               | Some _ -> (n,None)) ns
+
 let boardEmpty
     = (List.concat << List.mapi rowEmpty) >> List.filter (fun x -> match x with 
                                                                      | (_, Some _) -> true 
                                                                      | _ -> false)
-let rec replace n m
+
+let replace n m
     = List.mapi (fun i x -> if i=n 
                             then m 
                             else x)
+
 let newCell (t: int*int option) k
     = List.mapi (fun i (x:int option list) -> if i = fst t 
                                               then replace (snd t).Value (Some k) x 
                                               else x) 
-let rec game board (rnum:System.Random)
+
+let rec game board (rnum:System.Random) : unit
     = do 
         let key = System.Console.ReadKey().KeyChar
         let dir = match key with
@@ -65,7 +81,6 @@ let rec game board (rnum:System.Random)
         System.Console.Clear()
         ignore <| List.map (printfn "%s") (List.map rowformat newBoard)
         game newBoard rnum
-        ()
 
 [<EntryPoint>]
 let main argv = 
