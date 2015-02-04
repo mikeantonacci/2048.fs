17d53ad693b2bb6d660aeb6bd12f46b911c2f90c
michael antonacci <michaelantonacci2@gmail.com>
2015-02-03 22:50:20 -0500

    refactoring, typing, add signatures

--- 2048F/Program.fs
+++ 2048F/Program.fs
@@ -1,4 +1,6 @@
-﻿type cell = int option
+﻿module _2048
+
+type cell = int option
 type row = cell list
 type board = row list
 
@@ -7,6 +9,11 @@ let start : board = [[None;None;None;None];
                      [None;None;None;None];
                      [None;None;None;None];]
 let SIZE : int = 4
+
+let ap (f : ('a -> 'b) option) (x : 'a option) : 'b option
+    = match f with 
+        | Some _ -> match x with | Some _ -> Option.map f.Value x | None -> None
+        | None -> None
 
 let rec pad n xs = if List.length xs = n 
                    then xs 
@@ -25,7 +32,7 @@ let rec merge a
 let move : row -> row 
     = (shift <| SIZE) << List.rev << merge << List.rev << (SIZE |> shift)
 
-let rec transpose board
+let rec transpose board : board
     = match board with
         | [[];[];[];[]] -> []
         | _ -> List.map List.head board :: transpose (List.map List.tail board)
@@ -35,14 +42,22 @@ let moveLeft = List.rev << move << List.rev |> List.map
 let moveUp = transpose >> moveLeft >> transpose
 let moveDown = transpose << moveRight << transpose
 
+let moveDir key 
+    = match key with
+        | 'h' -> moveLeft
+        | 'j' -> moveDown
+        | 'k' -> moveUp
+        | 'l' -> moveRight
+        | _ -> id
+
 let cellFormat x
     = match x with 
         | Some n -> sprintf "%-4i" n 
         | None -> "    "
 
-let rowformat = List.reduce (fun x y -> x+"|"+y) << List.map cellFormat
+let rowformat = List.map cellFormat >> List.reduce (fun x y -> x+"|"+y) 
 
-let rowEmpty n ns
+let rowEmpty (n:int) (ns : row)
     = List.mapi (fun i x -> match x with 
                               | None -> (n,Some i) 
                               | Some _ -> (n,None)) ns
@@ -52,38 +67,37 @@ let boardEmpty
                                                                      | (_, Some _) -> true 
                                                                      | _ -> false)
 
-let replace n m
+let replace n (m : cell)
     = List.mapi (fun i x -> if i=n 
                             then m 
                             else x)
 
-let newCell (t: int*int option) k
+let newCell (t: int*cell) k
     = List.mapi (fun i (x:int option list) -> if i = fst t 
                                               then replace (snd t).Value (Some k) x 
                                               else x) 
 
-let win x = not (List.choose (List.tryFind (fun x -> x = Some 2048)) x).IsEmpty
+let isWin x = not (List.choose (List.tryFind (fun x -> x = Some 2048)) x).IsEmpty
+
+let newCellCoord r b
+    = List.nth (boardEmpty b) r
 
 let rec game board (rnum:System.Random) : unit
     = do 
         let key = System.Console.ReadKey().KeyChar
-        let dir = match key with
-                    | 'h' -> moveLeft
-                    | 'j' -> moveDown
-                    | 'k' -> moveUp
-                    | 'l' -> moveRight
-                    | _ -> id
         System.Console.Clear()
-        let movedBoard = dir board
-        if ((boardEmpty movedBoard).Length = 0) then gameOver true
-        let nxcl = (boardEmpty movedBoard).[rnum.Next <| (boardEmpty movedBoard |> List.length)]
+        let movedBoard = key |> moveDir <| board
+        if ((boardEmpty movedBoard).Length = 0) then gameOver <| true
         let k = if (rnum.Next 9) = 0 then 4 else 2
-        let newBoard = if board <> movedBoard || board = start then newCell nxcl k movedBoard  else board
-        ignore <| List.map (printfn "%s") (List.map rowformat movedBoard)
+        let i = rnum.Next <| (boardEmpty movedBoard |> List.length)
+        let newBoard = if board <> movedBoard || board = start
+                       then i |> newCellCoord <| movedBoard |> newCell <| k <| movedBoard
+                       else board
+        List.iter (printfn "%s") (List.map rowformat movedBoard)
         Async.Sleep 10000 |> ignore
         System.Console.Clear()
-        ignore <| List.map (printfn "%s") (List.map rowformat newBoard)
-        if win newBoard then gameOver false
+        List.iter (printfn "%s") (List.map rowformat newBoard)
+        if isWin newBoard then gameOver <| false
         game newBoard rnum
 
 and gameOver b 
---
+++ 2048F/Program.fsi
@@ -0,0 +1,29 @@
+﻿module _2048
+
+type cell = int option
+type row = cell list
+type board = row list
+val start : board
+val SIZE : int
+val ap : ('a -> 'b) option -> 'a option -> 'b option
+val pad : int -> 'a option list -> 'a option list
+val shift : int -> (row -> row)
+val merge : row -> row
+val move : (row -> row)
+val transpose : board -> board
+val moveRight : (board -> board)
+val moveLeft : (board -> board)
+val moveUp : (board -> board)
+val moveDown : (board -> board)
+val moveDir : char -> (board -> board)
+val cellFormat : cell -> string
+val rowformat : (row -> string)
+val rowEmpty : int -> row -> (int*cell) list
+val boardEmpty : (board -> (int*cell) list)
+val replace : int -> cell -> (row -> row)
+val newCell : (int * cell) -> int -> (board -> board)
+val isWin : board -> bool
+val newCellCoord : int -> board -> int*cell
+val game : board -> System.Random -> unit
+val gameOver : bool -> unit
+val main : string[] -> int
