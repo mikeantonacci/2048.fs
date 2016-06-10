2ca239dfd3429ff32097eed0521c17d7a2d589d5
Michael Antonacci <michaelantonacci2@gmail.com>
2016-06-09 20:52:36 -0400

    rename some params

--- 2048F/2048.fs
+++ 2048F/2048.fs
@@ -1,15 +1,13 @@
 ﻿namespace _2048
 
-open FSharpx.Collections.List
+open FSharpx.Collections
 open Aether
 open Aether.Operators
 
 type Direction = Up | Left | Right |Down
 
-module private __2048 =
+module private Game =
 
-    type 'a board = 'a option list list
-     
     let rec merge f xs
         = match xs with
             | (x :: y :: xs) when x = y -> (f x y) :: merge f xs
@@ -17,16 +15,16 @@ module private __2048 =
             | x -> x
 
     let move f (row : 'a option list) : 'a option list
-        = fill row.Length None << List.map Option.Some << merge f << List.choose id <| row
+        = List.fill row.Length None << List.map Option.Some << merge f << List.choose id <| row
 
     let moveLeft f
         = List.map (move f)
     let moveRight f
         = List.map (List.rev << move f << List.rev)
     let moveUp f
-        = transpose << moveLeft f << transpose
+        = List.transpose << moveLeft f << List.transpose
     let moveDown f
-        = transpose << moveRight f << transpose
+        = List.transpose << moveRight f << List.transpose
 
     let moveDir f dir
         = match dir with
@@ -35,7 +33,7 @@ module private __2048 =
             | Up -> moveUp f 
             | Right -> moveRight f 
 
-    let findOpenCells<'a when 'a : equality> : 'a board -> (int*int) list = 
+    let findOpenCells<'a when 'a : equality> : 'a option list list -> (int*int) list = 
         let rowEmpty (i : int) (ns : 'a option list) : (int*int) option list
             = List.mapi (fun j x -> match x with 
                                       | None -> Some (i, j) 
@@ -46,23 +44,21 @@ module private __2048 =
         Optic.set (List.pos_ i >?> List.pos_ j) << Option.Some
 
     let rowHasMerges<'a when 'a : equality> : 'a option list -> bool
-         = Seq.exists ((<||) (=)) << Seq.pairwise << List.toSeq
+         = Seq.exists <| (<||) (=) << Seq.pairwise << List.toSeq
 
     let boardHasMerges board = 
         let hasRowMerges = List.exists id << List.map rowHasMerges
-        hasRowMerges board || hasRowMerges <| transpose board 
+        hasRowMerges board || hasRowMerges <| List.transpose board 
 
-    let insertAtRandom (x,y) (rnum: System.Random) board
+    let insertAtRandom (a,b) (rnum: System.Random) board
         = let value = match rnum.Next 9 with
-                        | 0 -> y 
-                        | _ -> x
+                        | 0 -> b 
+                        | _ -> a
           let openCells = findOpenCells board
           let (i,j) = openCells.[rnum.Next openCells.Length] 
           insertNewCell (i,j) value board
 
-open __2048
-
-type Board<'a when 'a : equality> private (board:'a board, moveDir:Direction -> 'a board -> 'a board, values:'a*'a, size:int, win:'a, str:'a board -> string, rand:System.Random) = 
+type Board<'a when 'a : equality> private (board:'a option list list, moveDir:Direction -> 'a option list list -> 'a option list list, values:'a*'a, size:int, win:'a, str:'a option list list -> string, rand:System.Random) = 
 
     member val Board = board
 
@@ -70,10 +66,10 @@ type Board<'a when 'a : equality> private (board:'a board, moveDir:Direction -> 
         = Board(moveDir dir board, moveDir, values, size, win, str, rand)
 
     member this.InsertAtRandom
-        = Board(insertAtRandom values rand board, moveDir, values, size, win, str, rand)
+        = Board(Game.insertAtRandom values rand board, moveDir, values, size, win, str, rand)
 
     member this.HasNextMove
-        = not (List.isEmpty << findOpenCells <| board) || (boardHasMerges board)
+        = not (List.isEmpty << Game.findOpenCells <| board) || (Game.boardHasMerges board)
 
     member this.IsWin =
         List.exists id << List.map (List.exists ((=) <| Some win)) <| board
@@ -87,7 +83,7 @@ type Board<'a when 'a : equality> private (board:'a board, moveDir:Direction -> 
 
     override x.GetHashCode() = hash x.Board
 
-    static member construct(size:int, values:'a*'a, win:'a, op:'a -> 'a -> 'a, rand: System.Random, ?str:'a board -> string) : 'a Board =
+    static member construct(size:int, values:'a*'a, win:'a, op:'a -> 'a -> 'a, rand: System.Random, ?str:'a option list list -> string) : 'a Board =
         let board = List.init size (fun x -> List.init size (fun y -> Option<'a>.None))
         let toString = defaultArg str <| fun b -> b.ToString()
-        Board(board, moveDir op, values, size, win, toString, rand)
+        Board(board, Game.moveDir op, values, size, win, toString, rand)
